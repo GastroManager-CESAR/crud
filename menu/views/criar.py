a85b2c81ae03202816c7a74ce748bbf8bb6de273
giuliano <discordyy0@gmail.com>
2025-05-11 20:35:59 -0300

    feat(menu): organização das views

--- menu/views/criar.py
+++ menu/views/criar.py
@@ -1,13 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Item
-# Create your
-#  views here.
-
-def index(request):
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render())
-
+from ..models import Item
 def criar_item(request):
     if request.method == 'POST':
         form = request.POST
